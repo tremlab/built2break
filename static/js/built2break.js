995c37aad61e699c3d738d5704696c4a0f5c96fe
tremlab <rbalmert@gmail.com>
2018-07-11 15:19:15 -0700

    testing deploy

--- static/js/built2break.js
+++ static/js/built2break.js
@@ -3,8 +3,9 @@
 
 //  global value - but should be overwritten for each error.
 Bugsnag.releaseStage = "whatever";
+Bugsnag.appVersion = "1.3.1";
 
-document.addEventListener ("DOMContentLoaded", function() 
+document.addEventListener ("DOMContentLoaded", function()
     {
         // python error event listeners
         $('#beep').on('click', fBoop);
@@ -23,7 +24,7 @@ console.log("connected!!!!!!!");
 
 
 
-// severity    (optional) The seriousness of the error, 
+// severity    (optional) The seriousness of the error,
 // selected from the options info, warning and error, listed in order of increasing severity
 
 // Configuration for unhandled errors.
@@ -47,7 +48,7 @@ Bugsnag.notify("ErrorName", "Monkey pants!!!!!!1!!!!");
 
 // a basic JS error trigger, one time use
 function fBoop(evt) {
-    Bugsnag.notify("Beep", "Boop");
+    Bugsnag.notify("Beeeeep", "Boop");
     $('#beep').text("Boop");
     $('#beep').prop('disabled', true);
 }
@@ -87,9 +88,9 @@ function fJsRange(evt) {
     if (user_info["handling"] === "yes") {
         try {
             num.toPrecision(500);
-        } 
+        }
         catch (e) {
-            Bugsnag.notifyException(e, "a handled Range Error - ta da!!");            
+            Bugsnag.notifyException(e, "a handled Range Error - ta da!!");
         // action...?
         }
     }
@@ -106,9 +107,9 @@ function fJsRef(evt) {
     if (user_info["handling"] === "yes") {
         try {
             console.log(doesntExist);
-        } 
+        }
         catch (e) {
-            Bugsnag.notifyException(e, "a handled Reference Error - HUZZAH!");            
+            Bugsnag.notifyException(e, "a handled Reference Error - HUZZAH!");
         }
         // action...?
     }
@@ -125,16 +126,16 @@ function fJsType(evt) {
     if (user_info["handling"] === "yes") {
     var num = 1;
         try {
-            num.toUpperCase(); 
+            num.toUpperCase();
         }
         catch (e) {
-            Bugsnag.notifyException(e, "a handled Type Error - BOOYAH!");            
+            Bugsnag.notifyException(e, "a handled Type Error - BOOYAH!");
         }
             // action...?
     }
     else {
         // deliberate Type Error
-        num.toUpperCase(); 
+        num.toUpperCase();
     }
 }
 
@@ -154,7 +155,7 @@ function fShowError(payload) {
 function fIndex(evt) {
     var user_info = fGetUserData();
     console.log(user_info);
-    // sennding as get, not post - not form data, but a dictionary
+    // sending as get, not post - not form data, but a dictionary
     // fShowError not executing.  Success function doesn't return value.
     $.get('/index_error', user_info, fShowError);
 }
@@ -170,7 +171,3 @@ function fType(evt) {
     var user_info = fGetUserData();
     $.get('/type_error', user_info, fShowError);
 }
-
-
-
-
